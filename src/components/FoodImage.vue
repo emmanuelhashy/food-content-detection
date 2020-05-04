6751b0c92b8e178d67d074b9790efeacb200627f
Emmanuelhashy <emmanuelhashy@gmail.com>
2020-05-04 12:30:55 +0100

    All functionalities now working

---
+++ src/components/FoodImage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="img-detect">
+        <img :src="imageUrl" alt="" width="100%">
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FoodImage',
+  props: {
+      imageUrl: {
+          type: String
+      }
+  },
+//   setup(props) {
+//       const foodImageUrl = props.imageUrl;
+//       return{
+//           foodImageUrl
+//       }
+//   }
+  
+};
+</script>
+
+<style>
+
+</style>
